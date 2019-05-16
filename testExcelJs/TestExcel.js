9e77a30ab5b7ad2e85523a61779b4cc638be035a
TruongCongBach <truongcongbach1996@gmail.com>
2019-05-16 10:42:41 +0700

    init commit

---
+++ testExcelJs/TestExcel.js
@@ -0,0 +1,33 @@
+const Excel = require('exceljs');
+
+// create workbook & add worksheet
+const workbook = new Excel.Workbook();
+const worksheet = workbook.addWorksheet('Discography');
+
+// add column headers
+worksheet.columns = [
+    { header: 'Album', key: 'album'},
+    { header: 'Year', key: 'year'}
+];
+
+// add row using keys
+worksheet.addRow({album: "Taylor Swift", year: 2006});
+
+// add rows the dumb way
+worksheet.addRow(["Fearless", 2008]);
+
+// add an array of rows
+const rows = [
+  ["Speak Now", 2010],
+  {album: "Red", year: 2012}
+];
+worksheet.addRows(rows);
+
+// edit cells directly
+worksheet.getCell('A6').value = "1989";
+worksheet.getCell('B6').value = 2014;
+
+// save workbook to disk
+workbook.xlsx.writeFile('taylor_swift.xlsx').then(function() {
+  console.log("saved");
+});
